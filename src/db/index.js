6dc3baa5ddaab7a672caa9b506c39a277034fecc
iroda0103 <iroda3242@gmail.com>
2023-07-29 15:25:35 +0500

    databasaga seedlarni qo'sh

--- src/db/index.js
+++ src/db/index.js
@@ -1,4 +1,21 @@
 const mongoose = require('mongoose');
+const Admin=require('../modules/admins/Admin')
+const bcryptjs=require('bcryptjs')
+
+
+
+
+// module.exports = function (){
+//     return  mongoose.connection.dropDatabase()
+//     .then(() => {
+//       console.log('Database dropped successfully');
+//       // Perform any additional setup or initialization tasks here
+//     })
+//     .catch((error) => {
+//       console.error('Error dropping database:', error);
+//     });
+// }
+
 
 module.exports = function (){
     return  mongoose
@@ -12,3 +29,40 @@ module.exports = function (){
             console.log('Databazada xatolik' ,err);
         })
 }
+
+// Admin.deleteMany({})
+//   .then(() => {
+//     console.log('All data deleted successfully');
+//   })
+//   .catch((error) => {
+//     console.error('Error deleting data:', error);
+//   });
+
+// const seedAdmins=[
+//   {
+//     full_name:'Iroda Muminova',
+//     username:'iroda3242',
+//     password:bcryptjs.hashSync('1234'),
+//     is_super:true
+//   },
+//   {
+//     full_name:'Nurbek Muminov',
+//     username:'nurbek3242',
+//     password:bcryptjs.hashSync('1234'),
+//   },
+//   {
+//     full_name:'Ra\'no Muminova',
+//     username:'ra\'no3242',
+//     password:bcryptjs.hashSync('1234'),
+//   }
+
+// ]
+
+// const seedDB=async ()=>{
+//   await Admin.deleteMany({});
+//   await Admin.insertMany(seedAdmins)
+// }
+
+// seedDB().then(()=>{
+//   mongoose.connection.close();
+// })
