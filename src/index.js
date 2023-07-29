2347c8d813bd38940a905523b3753d2f3e4ef186
iroda0103 <iroda3242@gmail.com>
2023-07-29 15:19:45 +0500

    errorlarni boshqar

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 const express=require('express')
 const config=require('./shared/config')
 const db=require('./db')
-
+const router=require('./routes')
 const app=express()
 
 app.use(express.json())
+app.use(router)
 
 db()
 
---
+++ src/routes/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const adminRouter = require("../modules/admins/_api");
+const authorRouter = require("../modules/authors/_api");
+const bookRouter = require("../modules/books/_api");
+const borrowerRouter = require("../modules/borrowers/_api");
+const publisherRouter = require("../modules/publishers/_api");
+
+const router = express.Router();
+
+router.use(
+  adminRouter,
+  authorRouter,
+  bookRouter,
+  borrowerRouter,
+  publisherRouter
+);
+
+module.exports = router;
